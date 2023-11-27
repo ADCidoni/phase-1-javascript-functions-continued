398dd8bf89887d293560c3291aa76abc221a9b24
April <a.d.cidoni@gmail.com>
2023-11-26 19:08:49 -0500

    javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(someActivity = "roller-skate"){
+    return`This Saturday, I want to ${someActivity}!`
+}
+saturdayFun("bathe my dog");
+
+const mondayWork = function(someActivity = "go to the office"){
+    return `This Monday, I will ${someActivity}.`
+}
+mondayWork("work from home");
+function wrapAdjective (wrapper = "*") {
+    const innerFunction = function (someAdjective = "special") {
+        return `You are ${wrapper}${someAdjective}${wrapper}!`
+    }
+    return innerFunction
+}
+wrapAdjective("||")("a dedicated programmer")
